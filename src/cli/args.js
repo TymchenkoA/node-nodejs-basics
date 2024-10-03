79d4c430ef97600fd54ed767dd590e571eb34bd5
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-10-03 20:43:59 +0100

    feat: implement functions to practice work with command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    
+    const argNames = process.argv.filter((arg) => arg.startsWith('--'));
+
+    const filteredArguments = argNames.map((arg) => {
+        const argIndex = process.argv.indexOf(arg);
+
+        if (process.argv[argIndex + 1]) {
+            return `${arg.slice(2)} is ${process.argv[argIndex + 1]}`;
+        }    
+    });
+
+    console.log(filteredArguments .join(", "));
 };
 
 parseArgs();
