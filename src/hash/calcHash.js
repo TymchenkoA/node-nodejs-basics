267f6ebc78761a8f70abe137ebf4b08cc13aa3c6
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-10-04 11:37:27 +0100

    feat: implement function that calculates hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { getFileDirName } from "../utils/path.js";
+
+const __dirname = getFileDirName(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const input = createReadStream(
+    __dirname + "/files/fileToCalculateHashFor.txt"
+  );
+
+  input.on("readable", () => {
+    const data = input.read();
+
+    if (data) {
+        hash.update(data);
+    }
+    else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
