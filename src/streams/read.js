5ae707b17b277d3a62304a475a82643850d1bc0b
Anastasiia Tymchenko <tymchenko.anastasiia@gmail.com>
2024-10-04 13:20:58 +0100

    feat: implement functions to practice work with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from "fs";
+import os from "os";
+import { stdout } from "process";
+import { getFileDirName } from "../utils/path.js";
+
+const __dirname = getFileDirName(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  let data = "";
+  
+  const readStream = createReadStream(__dirname + "/files/fileToRead.txt", {
+    encoding: "utf8",
+  });
+
+  readStream.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  readStream.on("end", () => {
+    stdout.write(`${data}${os.EOL}`);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform, pipeline } from "stream";
+import { stdin, stdout } from "process";
+import os from "os";
+
 const transform = async () => {
-    // Write your code here 
+    
+  const reversedInput = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split("").reverse().join("") + os.EOL);
+    },
+  });
+
+  pipeline(stdin, reversedInput, stdout);
+
+  console.log("Please enter content you want to reverse");
 };
 
 await transform();
